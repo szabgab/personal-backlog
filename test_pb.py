a40246b8880b600cde2c13305e363c51f0fb6c1c
Gabor Szabo <gabor@szabgab.com>
2018-02-15 09:46:09 +0100

    pytest in class

--- test_pb.py
+++ test_pb.py
@@ -1,51 +1,52 @@
 import os
 import PersonalBacklog
 
-def test_pb(tmpdir, capsys):
-    filename = os.path.join(tmpdir, 'test.json')
-    pb = PersonalBacklog.PB(filename)
+class TestPB(object):
+    def test_pb(self, tmpdir, capsys):
+        filename = os.path.join(tmpdir, 'test.json')
+        pb = PersonalBacklog.PB(filename)
 
-    assert pb.list_calendar() == None
-    out, err = capsys.readouterr()
-    assert err == ''
-    assert out == ''
+        assert pb.list_calendar() == None
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == ''
 
-    assert pb.list_todo() == None
-    out, err = capsys.readouterr()
-    assert err == ''
-    assert out == ''
+        assert pb.list_todo() == None
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == ''
 
-    assert pb.add("Hello World", '1', '3') == None
-    out, err = capsys.readouterr()
-    assert err == ''
-    assert out == ''
+        assert pb.add("Hello World", '1', '3') == None
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == ''
 
-    assert pb.list_todo() == None
-    out, err = capsys.readouterr()
-    assert err == ''
-    assert out == '0) 3 - 1 - Hello World\n'
+        assert pb.list_todo() == None
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == '0) 3 - 1 - Hello World\n'
 
-    assert pb.add("Second Task", '3', '2') == None
-    out, err = capsys.readouterr()
-    assert err == ''
-    assert out == ''
+        assert pb.add("Second Task", '3', '2') == None
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == ''
 
-    assert pb.add("Third Thing", '0', '7') == None
-    out, err = capsys.readouterr()
-    assert err == ''
-    assert out == ''
+        assert pb.add("Third Thing", '0', '7') == None
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == ''
 
-    assert pb.list_todo() == None
-    out, err = capsys.readouterr()
-    assert err == ''
-    assert out == '0) 3 - 1 - Hello World\n1) 2 - 3 - Second Task\n2) 7 - 0 - Third Thing\n'
+        assert pb.list_todo() == None
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == '0) 3 - 1 - Hello World\n1) 2 - 3 - Second Task\n2) 7 - 0 - Third Thing\n'
 
-    assert pb.delete('1') == {}
-    out, err = capsys.readouterr()
-    assert err == ''
-    assert out == ''
+        assert pb.delete('1') == {}
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == ''
 
-    assert pb.list_todo() == None
-    out, err = capsys.readouterr()
-    assert err == ''
-    assert out == '0) 3 - 1 - Hello World\n1) 7 - 0 - Third Thing\n'
+        assert pb.list_todo() == None
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == '0) 3 - 1 - Hello World\n1) 7 - 0 - Third Thing\n'
