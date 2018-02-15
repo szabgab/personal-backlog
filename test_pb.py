83f61c5668bd62e76d980dd852255f19adb80cea
Gabor Szabo <gabor@szabgab.com>
2018-02-15 10:00:12 +0100

    delete

--- test_pb.py
+++ test_pb.py
@@ -51,11 +51,23 @@ class TestPB(object):
         pb.add("Hello World", '1', '3')
         pb.add("Second Task", '3', '2')
         pb.add("Third Thing", '0', '7')
+        pb.add("Fourth todo", '2', '2')
+
+        # delete before listing does not know what to delete
+        assert pb.delete('1') == {'error': "Invalid id - not in range '1'"}
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == ''
 
         assert pb.list_todo() == None
         out, err = capsys.readouterr()
         assert err == ''
-        assert out == '0) 3 - 1 - Hello World\n1) 2 - 3 - Second Task\n2) 7 - 0 - Third Thing\n'
+        assert out == '0) 3 - 1 - Hello World\n1) 2 - 3 - Second Task\n2) 7 - 0 - Third Thing\n3) 2 - 2 - Fourth todo\n'
+
+        assert pb.delete('4') == {'error': "Invalid id - not in range '4'"}
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == ''
 
         assert pb.delete('1') == {}
         out, err = capsys.readouterr()
@@ -65,4 +77,20 @@ class TestPB(object):
         assert pb.list_todo() == None
         out, err = capsys.readouterr()
         assert err == ''
-        assert out == '0) 3 - 1 - Hello World\n1) 7 - 0 - Third Thing\n'
+        assert out == '0) 3 - 1 - Hello World\n1) 7 - 0 - Third Thing\n2) 2 - 2 - Fourth todo\n'
+
+        assert pb.delete('1') == {}
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == ''
+
+        # TODO: maybe we need a better error message when the user tries to double-delete, or we need feedback for deletition?
+        assert pb.delete('1') == {'error': 'Internal error. Could not find it.'}
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == ''
+
+        assert pb.list_todo() == None
+        out, err = capsys.readouterr()
+        assert err == ''
+        assert out == '0) 3 - 1 - Hello World\n1) 2 - 2 - Fourth todo\n'
